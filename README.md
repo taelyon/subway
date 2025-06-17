a75739c359e29f6b8500de2cc4adf024f976cfcf
taelyon <121083945+taelyon@users.noreply.github.com>
2025-06-17 15:58:07 +0900

    Add SubwayArrivalApp SwiftUI example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# subway
+# Subway Repository
+
+이 저장소에는 한국 지하철 도착 정보를 보여주는 iOS 앱 예제 `SubwayArrivalApp`가 포함되어 있습니다. `SubwayArrivalApp` 디렉터리의 코드를 Xcode 프로젝트에 추가하여 사용하세요.
---
+++ SubwayArrivalApp/SubwayArrivalApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct SubwayArrivalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
